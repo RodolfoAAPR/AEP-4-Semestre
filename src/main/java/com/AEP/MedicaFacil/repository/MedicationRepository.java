a8b558a1d4d79e20da55983990db6453dc814590
RodolfoAAPR <165041838+RodolfoAAPR@users.noreply.github.com>
2025-11-10 14:35:53 -0300

    refactor(medication-repository): correct user to medication on parameter

--- src/main/java/com/AEP/MedicaFacil/repository/MedicationRepository.java
+++ src/main/java/com/AEP/MedicaFacil/repository/MedicationRepository.java
@@ -1,7 +1,7 @@
 package com.AEP.MedicaFacil.repository;
 
-import com.AEP.MedicaFacil.model.User;
+import com.AEP.MedicaFacil.model.Medication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MedicationRepository extends JpaRepository<User, Long> {
+public interface MedicationRepository extends JpaRepository<Medication, Long> {
 }
