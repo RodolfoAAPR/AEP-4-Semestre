75ec0d62dd4075abe4c2e1d18b8c1c5bb30d9320
RodolfoAAPR <165041838+RodolfoAAPR@users.noreply.github.com>
2025-11-10 10:54:47 -0300

    feat(medication-model): add medication attributes

---
+++ src/main/java/com/AEP/MedicaFacil/model/Medication.java
@@ -0,0 +1,26 @@
+package com.AEP.MedicaFacil.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Medication {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String medicationName;
+
+    private String description;
+}
