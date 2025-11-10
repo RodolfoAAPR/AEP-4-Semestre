680b4189f6fa4dd8002f3f048b4df79cf5cc05fa
RodolfoAAPR <165041838+RodolfoAAPR@users.noreply.github.com>
2025-11-10 10:37:20 -0300

    feat(user-model): add user attributes

---
+++ src/main/java/com/AEP/MedicaFacil/MedicaFacilApplication.java
@@ -0,0 +1,13 @@
+package com.AEP.MedicaFacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicaFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicaFacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AEP/MedicaFacil/MedicaFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AEP.MedicaFacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicaFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
