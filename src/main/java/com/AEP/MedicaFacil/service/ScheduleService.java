7c182ed442e91dfdccac6bdbf034537de13b3e0b
RodolfoAAPR <165041838+RodolfoAAPR@users.noreply.github.com>
2025-11-10 14:21:06 -0300

    feat(schedule-service): add methods to update, find all, find by id and delete user

---
+++ src/main/java/com/AEP/MedicaFacil/service/ScheduleService.java
@@ -0,0 +1,49 @@
+package com.AEP.MedicaFacil.service;
+
+import com.AEP.MedicaFacil.model.Schedule;
+import com.AEP.MedicaFacil.repository.ScheduleRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class ScheduleService {
+
+    @Autowired
+    private ScheduleRepository scheduleRepository;
+
+    public List<Schedule> findAllSchedules(){
+        return scheduleRepository.findAll();
+    }
+
+    public Schedule findScheduleById(Long id){
+        return scheduleRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Agendamento com o ID: " + id + " não encontrado!"));
+    }
+
+    public Schedule registerSchedule(Schedule schedule){
+        return scheduleRepository.save(schedule);
+    }
+
+    public Schedule updateSchedule(Schedule schedule, Long id){
+        Schedule newSchedule = scheduleRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Agendamento com o ID:" + id + " não encontrado!"));
+
+        newSchedule.setDosage(newSchedule.getDosage());
+        newSchedule.setTime(newSchedule.getTime());
+        newSchedule.setFrequency(newSchedule.getFrequency());
+        newSchedule.setStartDate(newSchedule.getStartDate());
+        newSchedule.setFinishDate(newSchedule.getFinishDate());
+
+        return scheduleRepository.save(newSchedule);
+    }
+
+    public void deleteSchedule(Long id){
+        scheduleRepository.deleteById(id);
+    }
+}
+
