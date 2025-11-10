c2a2fc951b56ebd191d85c716cb09035b4b73961
RodolfoAAPR <165041838+RodolfoAAPR@users.noreply.github.com>
2025-11-10 11:15:01 -0300

    refactor(medication-model): add colored terminal and hibernate check

--- src/main/java/com/AEP/MedicaFacil/model/Medication.java
+++ src/main/java/com/AEP/MedicaFacil/model/Medication.java
@@ -18,7 +18,7 @@ public class Medication {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long idMedication;
 
     private String medicationName;
 
